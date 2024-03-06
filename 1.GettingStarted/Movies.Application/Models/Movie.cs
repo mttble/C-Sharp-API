89b43d8163142c5b08708cd40dca8c1f2a6336ca
Matthew Beale <129032908+mttble@users.noreply.github.com>
2024-03-06 11:59:45 +1030

    Added slug identifier to endpoint

--- 1.GettingStarted/Movies.Application/Models/Movie.cs
+++ 1.GettingStarted/Movies.Application/Models/Movie.cs
@@ -1,13 +1,26 @@
-﻿namespace Movies.Application.Models;
+﻿using System.Text.RegularExpressions;
 
-public class Movie
+namespace Movies.Application.Models;
+
+public partial class Movie
 {
     public required Guid Id { get; set; }
     
     public required string Title { get; set; }
     
+    public string Slug => GenerateSlug();
+    
     public required int YearOfRelease { get; set; }
     
     public required List<string> Genres { get; init; } = new();
+    
+    private string GenerateSlug()
+    {
+       var sluggedTitle = SlugRegex().Replace(Title, string.Empty).ToLower().Replace(" ", "-");
+       return $"{sluggedTitle}-{YearOfRelease}";
+    }
+    [GeneratedRegex("[^a-zA-Z0-9 _-]", RegexOptions.NonBacktracking, 5)]
+    private static partial Regex SlugRegex();
+    
 
 }
